c8bcc2af908498d6b5d853ca9e7d505199a4dca7
Djperrefort <djperrefort@gmail.com>
2016-07-14 11:37:15 -0400

    Progress update
    - Changed check file type from wt to exp
    - Minor edits to ‘Unpack Directory.ipynb’
    - Added ‘Unpack Directory.py’

---
+++ Code Snippets/Unpack Directory.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import zipfile, os, gzip
+directory = "."
+    
+for path, subdirs, files in os.walk(directory):
+    for name in files:
+        if ".gz" in name:
+
+            inF = gzip.open(os.path.join(path, name), 'rb')
+            data = inF.read()
+            inF.close()
+
+            outF = open(os.path.join(path, name).replace('.gz',''), 'wb')
+            outF.write(data)
+            outF.close()
+            
+            os.remove(os.path.join(path, name))
+            
+        elif ".zip" in name:
+            zip_ref = zipfile.ZipFile(os.path.join(path, name), 'r')
+            zip_ref.extractall(path)
+            zip_ref.close()
+            
+            os.remove(os.path.join(path, name))
+
